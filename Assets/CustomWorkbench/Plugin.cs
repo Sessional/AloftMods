89212d2bfcd533a55f931fddbcc9aa5f2a822fbc
Sessional <Sessional@users.noreply.github.com>
2025-01-27 21:22:14 -0600

    add a new custom workbench mod to demonstrate new crafting stations

---
+++ Assets/CustomWorkbench/Plugin.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Unity.Mono;
+using UnityEngine;
+
+[BepInPlugin("customworkbenches.sessional.dev", "customworkbenches", "1.0")]
+public class Plugin : BaseUnityPlugin
+{
+}
